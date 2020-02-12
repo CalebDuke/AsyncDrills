b50ee13d7a74e49b25cefd36e0ff5f67f9f887ff
Caleb Duke <dukecj18@gmail.com>
2020-02-12 16:58:15 -0600

    Hey

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// function messageString (message) {
+//     return message
+// }
+
+// let myMessage = messageString ("this is a string");
+// setTimeout(function(){console.log(myMessage)}, 2000)
+
+// function getWord(word) {
+//     return word
+// }
+
+// let delayWord1 = getWord ("word1")
+// let delayWord2 = getWord ("word2")
+// let delayWord3 = getWord ("word3")
+// let delayWord4 = getWord ("word4")
+// setTimeout(function(){console.log(delayWord1)})
+// setTimeout(function(){console.log(delayWord2)}, 2000)
+// setTimeout(function(){console.log(delayWord3)}, 3000)
+// setTimeout(function(){console.log(delayWord4)}, 4000)
+
+
+
+function done () {
+        return message
+};
+
+
+
+
+
+
+
+
+
