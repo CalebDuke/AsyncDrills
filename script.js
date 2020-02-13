bdd621e0bf49b46f84c40f0fe52b4e7ba229c84c
Caleb Duke <dukecj18@gmail.com>
2020-02-13 11:24:52 -0600

    boi

--- script.js
+++ script.js
@@ -21,15 +21,42 @@
 
 
 
-function done () {
-        return message
-};
+
+// let done = () => (console.log("Job's done!"));
+// // console.log(job());
+
+// function countDown(num, callback) {
+//         if (num > 0) {
+//                 console.log(num);
+//                 setTimeout(() => {
+//                         countDown(num - 1, callback)
+//                 }, 1000);
+//         } else {
+//                 callback();
+//         }
+// }
+
+// countDown(4, done);
 
 
+let lunchTime = true;
 
+let orderMeSomeFood = (message) => {
+        return new Promise((resolve, reject) => {
+                 if (lunchTime) {
+                         let lunchObj = {
+                                 lunch: "Pizza",
+                                 drink: "Beer"
+                         }
+                         resolve(lunchObj)
+                 } else {
+                         let err = new Error('ew');
+                         reject(err)
+                 }
 
-
-
-
-
+        })
+}
+orderMeSomeFood(lunchTime)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
 
